11b8762c607cf0488e01652c1792ba0ce13414b5
Selahattin Toprak <selahattin47usa@hotmail.com>
2020-04-09 22:50:36 -0700

    added navbar actions to admin pages

--- components/AdminPage/AdminPage.js
+++ components/AdminPage/AdminPage.js
@@ -1,11 +1,12 @@
 import Page from "../Page";
 import Layout from "../layout/Layout";
 import { menuItems } from "../../src/data/admin/menu_items";
+import NavbarActions from "./NavbarActions/NavbarActions";
 
 export default ({ children, title, description }) => (
   <>
     <Page title={title} description={description}>
-      <Layout menuItems={menuItems} logoLink="/admin">
+      <Layout menuItems={menuItems} logoLink="/admin" navbarActions={<NavbarActions />}>
         {children}
       </Layout>
     </Page>
