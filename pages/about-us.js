22022878abcca0591301a5bc9a02034f7ca0b287
Selahattin Toprak <selahattin47usa@hotmail.com>
2020-04-09 22:33:39 -0700

    fixed about us page and added some content to admin page

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,17 +1,13 @@
 import Quotes from "../components/Quotes/Quotes";
-import Page from "../components/Page";
-import Layout from "../components/layout/Layout";
+import ClientPage from "../components/ClientPage/ClientPage";
 
 export const config = { amp: true };
 export default () => (
   <>
-    <Page title="Moving Deals" description="Compare Moving Deals & Quotes">
-      <Layout>
+    <ClientPage title="Moving Deals" description="Compare Moving Deals & Quotes">
         <section className="quotes">
           <h1>Book your move online</h1>
-          <Quotes></Quotes>
         </section>
-      </Layout>
-    </Page>
+    </ClientPage>
   </>
 );
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,13 +1,75 @@
 import AdminPage from "../../components/AdminPage/AdminPage";
+import { CardsStyles } from "../../ui-styles/styles";
 
 export const config = { amp: true };
 
 export default () => (
   <>
     <AdminPage title="Moving Deals" description="Compare Moving Deals & Quotes">
-      <section className="quotes">
-        <h1>Book your move online</h1>
+      <section className="section section-banner">
+        <div className="container">
+          <h1
+            className="text-center"
+            style={{
+              fontSize: "1.75rem",
+              fontWeight: "bold",
+
+              marginTop: "4rem",
+            }}
+          >
+            Moving.Deals
+          </h1>
+          <div className="columns">
+            <div className="column col-6 col-xs-12">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-title h5">Inventory</div>
+                </div>
+                <div className="card-body">Manage the inventory items.</div>
+                <div className="card-footer">
+                  <a className="btn btn-primary" href="components/accordions.html">
+                    View
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="column col-6 col-xs-12">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-title h5">Moves</div>
+                </div>
+                <div className="card-body">View and manage all moves.</div>
+                <div className="card-footer">
+                  <a className="btn btn-primary" href="components/avatars.html">
+                    View
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </AdminPage>
+    <CardsStyles />
+    <style jsx global>
+      {`
+        @media (min-width: 960px) {
+          .section-banner .container .columns {
+            width: 50%;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        }
+        .card {
+          box-shadow: 0 0.25rem 1rem rgba(0, 0, 0, 0.2);
+          border: 0;
+          height: 100%;
+          margin: 2px;
+        }
+        .column {
+          padding: 16px;
+        }
+      `}
+    </style>
   </>
 );
